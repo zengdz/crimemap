6ad8b65825ffc55fe6b572a2a42a2ae1700194c4
zengdz <zeng.zhen@foxmail.com>
2016-10-17 16:52:35 +0800

    CrimeMap框架形成
    crimemap是主程序，dbhelper是封装处理SQL函数，还有前端显示交互处理。注意这个项目处理不同URL的方法和之前不同，之前是把POST收集然后在根目录全部处理，现在是在前端不同操作URL定位到不同目录，主程序有对应URL函数。

---
+++ dbhelper.py
@@ -0,0 +1,46 @@
+#coding: utf-8
+import pymysql
+import dbconfig
+
+#所有的数据操作都放在try-finally里面，以便数据库最终都能顺利关闭连接
+class DBHelper:
+
+	def connect(self, database="crimemap"):
+		return pymysql.connect(host='localhost',
+		user=dbconfig.db_user,
+		passwd=dbconfig.db_password,
+		db=database)
+
+	#Reading data
+	def get_all_inputs(self):
+		connection = self.connect() #每次操作之前都要先和数据库建立连接
+		try:
+			query = "SELECT description FROM crimes;" #从crimes数据库选择description数据，注意最后有分号，应该是SQL的语法
+			with connection.cursor() as cursor: #使用with-as
+				cursor.execute(query) #指针执行这个请求之后，cursor就指向所需数据了。
+			return cursor.fetchall() #使用fetchall方法把指向的数据变成Python能处理的列表数据
+		finally:
+			connection.close()
+
+	#Inserting data
+	def add_input(self, data):
+		connection = self.connect()
+		try:
+			# The following introduces a deliberate security flaw. See section on SQL injection below
+			query = "INSERT INTO crimes (description) VALUES ('{}');".format(data) # VALUES后面括号内数据对应的是前面括号内容
+			with connection.cursor() as cursor:
+				cursor.execute(query) #执行请求操作
+				connection.commit() #不同于读取数据，插入数据对数据库修改了所以要提交修改才能生效
+		finally:
+			connection.close()
+
+	def clear_all(self):
+		connection = self.connect()
+		try:
+			query = "DELETE FROM crimes;" #删除crimes数据库的所有内容
+			with connection.cursor() as cursor:
+				cursor.execute(query) #执行请求操作
+				connection.commit() #删除数据对数据库修改了所以要提交修改才能生效
+		finally:
+			connection.close()
+
